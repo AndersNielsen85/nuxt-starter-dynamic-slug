722caf2530c476fb70da91c8d9737932d2c06a90
AndersNielsen85 <anders.b.nielsen85@gmail.com>
2022-08-15 14:16:00 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# nuxt-starter-aj6mea
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/nuxt-starter-aj6mea)
---
+++ pages/[...slug].vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    Catch all route
+    <div>
+      <NuxtLink to="/1">1</NuxtLink><br />
+      <NuxtLink to="/2">2</NuxtLink><br />
+      <NuxtLink to="/non-existing-page">404</NuxtLink>
+      <button @click="refresh">Refresh data</button>
+    </div>
+    Data based on route {{ route.params.slug }}
+    {{ data }}
+  </div>
+</template>
+<script setup lang="ts">
+const route = useRoute();
+
+console.log(route.params.slug);
+const { data, refresh } = await useFetch(
+  'https://jsonplaceholder.typicode.com/posts/' + route.params.slug
+);
+
+//onMounted(() => {
+//  refresh();
+//});
+</script>
